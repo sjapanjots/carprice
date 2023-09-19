0b4db0c840bc7cb53291723c15e0836a072a39e4
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-09-20 01:41:41 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import random
 
 st.title("Car Price Prediction Model Using ML")
 
@@ -13,19 +12,26 @@ car_owner_type = st.text_input("Car Model (by owner type) (first=0, second=1, th
 car_mileage = st.text_input("Car Model (by mileage)")
 car_engine = st.text_input("Car Model (by engine)")
 
-# Function to generate and display the random number
-def generate_random_number():
+# Function to predict and display the car price
+def predict_car_price():
     # Check if all input fields are filled
     if not all([car_year, car_company, car_fuel_type, car_seller_type, car_transmission_type, car_owner_type, car_mileage, car_engine]):
         st.warning("Please fill in all the car model features before predicting the price.")
     else:
-        # Generate a random number as the predicted car price
-        random_number = random.randint(100000, 300000)
-        st.write("Predicted Price of the Car is", random_number)
+        # Replace this placeholder with your actual prediction logic or ML model
+        # Here, we are using a simple formula as a placeholder
+        price = 100000 + (int(car_year) * 20) + (int(car_company) * 50)
+        # Ensure the predicted price falls within the range of 100,000 to 300,000
+        price = max(100000, min(price, 300000))
+        st.write("Predicted Price of the Car is $", price)
 
 # Create a button to trigger the car price prediction
 if st.button("Predict The Car Price"):
-    generate_random_number()
+    predict_car_price()
+
+# Footer
+st.markdown("---")
+st.write("Designed and Developed by Piyush Batra")
 
 # Footer
 st.markdown("---")
